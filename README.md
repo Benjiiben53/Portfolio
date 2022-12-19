76496f5466fec4f2b87a9b842c7d3385547d4a4c
Benjiiben53 <benjamin.fichot.53@gmail.com>
2022-12-20 00:14:44 +0100

    Tu me rerange tout ça oui

--- README.md
+++
@@ -1,3 +0,0 @@
-# HTML-CSS
-
-Voilà le tout !
--- portfolio/src/presets/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div>
-        <p>Code source du site sur mon <a href="https://github.com/Benjiiben53/Portfolio">github<i
-                    class="fa-brands fa-github"></i></a></p>
-    </div>
-</footer>
